a32ca047efeafebc6aaa908393970ebf130f35d0
Anil Singh <anil927a@gmail.com>
2022-12-19 17:52:57 +0530

    todoList

---
+++ src/component/todo/todo.js
@@ -0,0 +1,71 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
+import { Fragment } from "react";
+import './todo.css'
+
+
+
+function TodoList() {
+    const todoList = [
+        { name: "project1", isDeleted: false, isCompleted: false },
+        { name: "project2", isDeleted: false, isCompleted: false },
+        { name: "project3", isDeleted: false, isCompleted: false },
+        { name: "project4", isDeleted: false, isCompleted: false }]
+    let [count, setCount] = useState(0)
+    let [list, setlist] = useState(todoList)
+    let [value, setValue] = useState('')
+
+    function Increment() {
+        setCount(count++);
+    }
+
+    function addList() {
+        const newTodo = {
+            name: value,
+            isDeleted: false,
+            isCompleted: false
+        }
+        setlist([...list, newTodo])
+        console.log(list)
+        setValue('')
+    }
+
+    function captureUserInput(event) {
+        setValue(event.target.value)
+    }
+
+    function completeTodo(item) {
+        const indexOfClickedTodo = list.findIndex(Nm => Nm.name === item.name)
+        console.log(item, indexOfClickedTodo)
+        list[indexOfClickedTodo].isCompleted = !list[indexOfClickedTodo].isCompleted
+        setlist([...list])
+        console.log(list)
+    }
+
+    function deleteTodo(item) {
+        const indexOfClickedTodo = list.findIndex(Nm => Nm.name === item.name)
+        list.splice(indexOfClickedTodo, indexOfClickedTodo);
+        setlist([...list])
+    }
+
+    return (
+        <Fragment>
+            <div className="outerDiv" >
+                <div className="innerDiv">
+                    <h1 style={{ color: 'white' }} >Pending tasks({count})</h1>
+                    <ul>
+                        {list.map((item) => (<div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                            <li className="TodoList" style={{ color: 'white', textDecoration: item.isCompleted ? 'line-through' : 'none' }}>{item.name}</li>
+                            <button onClick={() => completeTodo(item)} id="complete" style={{ borderRadius: '10px', border: '3px solid green' }}>{item.isCompleted ? 'Restart' : 'Complete'}</button>
+                            <button onClick={() => deleteTodo(item)} id="delete" style={{ borderRadius: '10px', border: '3px solid red' }}>Delete</button>
+                        </div>))}
+                    </ul>
+                    <input onChange={captureUserInput} value={value} placeholder="Add new task"></input>
+                    <button onClick={Increment} onClick={addList} value={value}>Add-Item</button>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default TodoList
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './component/counter/counter';
+import Todo from './component/todo/todo';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Counter/>
+    <Todo/>
   </React.StrictMode>
 );
 
